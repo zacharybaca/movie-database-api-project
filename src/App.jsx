d0263ed9b2b620bb4d5ef13a6e8f3308d66971f3
Zachary Baca <zachary.baca@msn.com>
2024-07-31 12:20:12 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,7 @@
+import GeneratedPassword from "./components/GeneratedPassword/GeneratedPassword";
+import PasswordCriteria from "./components/PasswordCriteria/PasswordCriteria";
 import { PasswordGeneratorContextProvider } from './context/passwordGeneratorContext';
+import { PasswordCriteriaContextProvider } from './context/passwordCriteriaContext';
 import './App.css'
 
 function App() {
@@ -7,8 +10,12 @@ function App() {
   return (
     <div id="app-container">
       <PasswordGeneratorContextProvider>
-
+        <GeneratedPassword />
       </PasswordGeneratorContextProvider>
+      
+      <PasswordCriteriaContextProvider>
+        <PasswordCriteria />
+      </PasswordCriteriaContextProvider>
     </div>
   )
 }
