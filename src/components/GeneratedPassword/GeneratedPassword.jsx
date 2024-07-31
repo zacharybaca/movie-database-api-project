ce21a07b344b1336d3c261dd682c9cf4bd7571c8
Zachary Baca <zachary.baca@msn.com>
2024-07-31 12:08:47 -0500

    Update GeneratedPassword.jsx

--- src/components/GeneratedPassword/GeneratedPassword.jsx
+++ src/components/GeneratedPassword/GeneratedPassword.jsx
@@ -1,9 +1,12 @@
+import { PasswordGeneratorContextProvider } from '../../context/passwordGeneratorContext';
 import './generated-password.css';
 
 export default function GeneratedPassword() {
     return (
         <div id="password-container">
-
+            <PasswordGeneratorContextProvider>
+                
+            </PasswordGeneratorContextProvider>
         </div>
     )
 }
