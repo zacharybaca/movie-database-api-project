dc2131b1b372d6bf6552e191daa908340255afd3
Zachary Baca <zachary.baca@msn.com>
2024-07-31 12:19:41 -0500

    Update PasswordCriteria.jsx

--- src/components/PasswordCriteria/PasswordCriteria.jsx
+++ src/components/PasswordCriteria/PasswordCriteria.jsx
@@ -1,12 +1,9 @@
-import { PasswordCriteriaContextProvider } from '../../context/passwordCriteriaContext';
 import './password-criteria.css';
 
 export default function PasswordCriteria() {
     return (
         <div id="password-criteria-container">
-            <PasswordCriteriaContextProvider>
-                
-            </PasswordCriteriaContextProvider>
+            
         </div>
     )
 }
