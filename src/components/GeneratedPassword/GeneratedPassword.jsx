662a61916b2062be1bc04102451cefe2bcc6f5b5
zacharybaca <zachary.baca@msn.com>
2024-07-31 17:23:53 -0500

    connected context to components

--- src/components/GeneratedPassword/GeneratedPassword.jsx
+++ src/components/GeneratedPassword/GeneratedPassword.jsx
@@ -1,9 +1,13 @@
+import React from "react";
+import { GeneratedPasswordContext } from "../../context/passwordGeneratorContext";
 import './generated-password.css';
 
 export default function GeneratedPassword() {
+    const context = React.useContext(GeneratedPasswordContext);
+    
     return (
         <div id="password-container">
-            
+            <h1>Generated Password</h1>
         </div>
     )
 }
--- src/components/PasswordCriteria/PasswordCriteria.jsx
+++ src/components/PasswordCriteria/PasswordCriteria.jsx
@@ -1,9 +1,13 @@
+import React from "react";
+import { PasswordCriteriaContext } from "../../context/passwordCriteriaContext";
 import './password-criteria.css';
 
 export default function PasswordCriteria() {
+    const context = React.useContext(PasswordCriteriaContext);
+    
     return (
         <div id="password-criteria-container">
-            
+            <h1>Password Criteria</h1>
         </div>
     )
 }
