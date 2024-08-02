3796acb4494378815d2dccaa8475b5959e5a8534
zacharybaca <zachary.baca@msn.com>
2024-08-02 17:47:35 -0500

    styled password-criteria component

--- src/components/PasswordCriteria/PasswordCriteria.jsx
+++ src/components/PasswordCriteria/PasswordCriteria.jsx
@@ -7,7 +7,24 @@ export default function PasswordCriteria() {
     
     return (
         <div id="password-criteria-container">
-            <h1>Password Criteria</h1>
+            <h1>Select Your Password Requirements</h1>
+            <form id="criteria-form" name="criteria-form">
+                <div id="numbers-container">
+                    <label htmlFor="numbers">Require Numbers </label>
+                    <input type="checkbox" id="numbers" name="numbers" />
+                </div>
+                <div id="special-container">
+                    <label htmlFor="special">Require Special Characters </label>
+                    <input type="checkbox" id="special" name="special" />
+                </div>
+                <div id="amount-container">
+                    <label htmlFor="amount">Number of Characters Required </label>
+                    <input type="range" id="amount" name="amount" min="5" max="20" step="1" />
+                </div>
+            </form>
+            <div id="button-container">
+                <button type="button">Generate Secure Password</button>
+            </div>
         </div>
     )
 }
